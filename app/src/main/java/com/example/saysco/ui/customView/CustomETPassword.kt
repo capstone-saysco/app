a5b308a96cb55219513c63a7c0698624c36c508a
Luthfia39 <you@example.com>
2023-12-06 04:56:56 +0700

    add custom view to check the inputs

---
+++ app/src/main/java/com/example/saysco/ui/customView/CustomETPassword.kt
@@ -0,0 +1,44 @@
+package com.example.saysco.ui.customView
+
+import android.content.Context
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.content.ContextCompat.getString
+import com.example.saysco.R
+
+class CustomETPassword : AppCompatEditText {
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        init()
+    }
+
+    private fun init() {
+
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                if (s.toString().length < 8) {
+                    setError(getString(context, R.string.pw_error), null)
+                } else {
+                    error = null
+                }
+            }
+
+            override fun afterTextChanged(s: Editable) {}
+        })
+    }
+}
