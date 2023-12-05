7e5c1ca6060333cef6ebefe044eb78d40b5382c6
Luthfia39 <you@example.com>
2023-12-05 22:51:40 +0700

    add new activities

---
+++ app/src/main/java/com/example/saysco/ui/input/InputActivity.kt
@@ -0,0 +1,12 @@
+package com.example.saysco.ui.input
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.saysco.R
+
+class InputActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_input)
+    }
+}
---
+++ app/src/main/java/com/example/saysco/ui/profile/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.example.saysco.ui.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.saysco.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
